2a8d4c7d92e34786107da8324a9da1decf2027d0
MustafaKashif <kashifmustafa822@gmail.com>
2022-09-30 20:58:05 +0500

    Mustafa Kashif

---
+++ public/app.js
@@ -0,0 +1,25 @@
+function cl(a){
+    var text = document.getElementById("oval");
+    text.className = "square";
+}
+
+function sq(a){
+    var text = document.getElementById("oval");
+    text.className = "circle";
+}
+
+function rec(a){
+    var text = document.getElementById("oval");
+    text.className = "parallelogram"
+}
+
+function para(a){
+    var text = document.getElementById("oval");
+    text.className = "rectangle"
+}
+
+function tri(a){
+    var text = document.getElementById("oval");
+    text.className = "triangle"
+}
+
